222566adfe5e7696d00211b213417d02ddf2e65a
Viktor <vicgerrard@gmail.com>
2016-12-19 00:32:50 +0300

    fixed connection

--- include/connection.php
+++ include/connection.php
@@ -5,6 +5,7 @@
  * Date: 18.12.2016
  * Time: 3:12
  */
+                   //host        login  password database name
 $conn = new mysqli('localhost', 'root', '', 'WEB');
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
